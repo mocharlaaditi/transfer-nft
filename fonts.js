850b6199791bee3d3219376194e0abf714fb9de4
Aditi Mocharla <aditi_mocharla@Aditis-MacBook-Pro.local>
2022-03-18 16:58:22 -0400

    initial commit

---
+++ fonts.js
@@ -0,0 +1,12 @@
+@font-face {
+    font-family: "circular Std";
+    src: url(https://media-cdn.getbento.com/fonts/circular/${src}.eot?#iefix) format('embedded-opentype'),
+              url(https://media-cdn.getbento.com/fonts/circular/${src}.otf) format('opentype'),
+               url(https://media-cdn.getbento.com/fonts/circular/${src}.woff) format('woff'),
+               url(https://media-cdn.getbento.com/fonts/circular/${src}.woff2) format('woff2'),
+               url(https://media-cdn.getbento.com/fonts/circular/${src}.ttf) format('truetype'),
+               url(https://media-cdn.getbento.com/fonts/circular/${src}.svg#${name}) format('svg');x
+    font-style: ${fontStyle};
+    font-weight: ${fontWeight};
+    font-display: swap;
+  }
